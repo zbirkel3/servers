9d038a71854412495f0d3f57449402ded895c6ca
unknown <zbirkel854@wit-xdvm-055.MILLARDPS.NET>
2012-11-16 11:05:49 -0600

    set up files and index.php is done

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		<title>SERVERS</title>
+	</head>
+	<body>
+		<div class="conatiner">
+			<header>
+				<?php include 'layout/header.php';?>
+			</header>
+			<nav>
+				<?php include 'layout/nav.php';?>
+			</nav>
+			<div id="content">
+				<?php include 'layout/content.php';?>
+			</div>
+			<footer>
+				<?php include 'layout/footer.php';?>
+			</footer>
+		</div>
+	</body>
+</html>	
